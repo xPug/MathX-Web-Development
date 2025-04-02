59b34ae51ec319a2f5fa92ee40d923c39070cec8
Aenimen <songuyen150@gmail.com>
2025-04-01 22:56:13 -0700

    Added logo, does not appear yet, only added function in HomePane.java

--- HomePane.java
+++ HomePane.java
@@ -8,5 +8,25 @@ public class HomePane extends GraphicPane {
 		this.mainScreen = mainScreen;
 	}
 	
+	@Override
+	public void showContent() {
+		addLogo();
+	}
 	
+	@Override
+	public void hideContent() {
+		for(GObject item : contents) {
+			mainScreen.remove(item);
+		}
+		contents.clear();
+	}
+	
+	private void addLogo() {
+		GImage logoImage = new GImage("MathXlogo.png", 100, 100);
+		logoImage.scale(1, 1);
+		logoImage.setLocation((mainScreen.getWidth() - logoImage.getWidth()) / 2, (mainScreen.getHeight() - logoImage.getHeight()) / 2);
+		
+		contents.add(logoImage);
+		mainScreen.add(logoImage);
+	}
 }
