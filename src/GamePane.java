52c7bce0460f0223bcbcb9c2cbc3d106cb1697e6
Aenimen <songuyen150@gmail.com>
2025-04-07 15:53:20 -0700

    Added showContent and hideContent in GamePane class

--- src/GamePane.java
+++ src/GamePane.java
@@ -3,6 +3,20 @@ import java.awt.event.MouseEvent;
 
 import acm.graphics.*;
 
-public class GamePane {
+public class GamePane extends GraphicPane {
 	
+	public GamePane(MainApplication mainScreen) {
+		this.mainScreen = mainScreen;
+	}
+	
+	@Override
+	public void showContent() {}
+	
+	@Override
+	public void hideContent() {
+		for(GObject item : contents) {
+			mainScreen.remove(item);
+		}
+		contents.clear();
+	}
 }
