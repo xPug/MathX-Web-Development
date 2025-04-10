056fc4cd0eb63ab189c69b28f30a52837acfac59
Aenimen <songuyen150@gmail.com>
2025-04-09 18:07:46 -0700

    Made background black for game page

--- src/GamePane.java
+++ src/GamePane.java
@@ -4,13 +4,16 @@ import java.awt.event.MouseEvent;
 import acm.graphics.*;
 
 public class GamePane extends GraphicPane {
+	private GRect backGround;
 	
 	public GamePane(MainApplication mainScreen) {
 		this.mainScreen = mainScreen;
 	}
 	
 	@Override
-	public void showContent() {}
+	public void showContent() {
+		backGround();
+	}
 	
 	@Override
 	public void hideContent() {
@@ -18,5 +21,14 @@ public class GamePane extends GraphicPane {
 			mainScreen.remove(item);
 		}
 		contents.clear();
+	}
+	
+	public void backGround() {
+		backGround = new GRect(mainScreen.getWidth(), mainScreen.getHeight());
+	    backGround.setFilled(true);
+	    backGround.setColor(java.awt.Color.BLACK);
+	    
+	    contents.add(backGround);
+	    mainScreen.add(backGround);
 	}
 }
