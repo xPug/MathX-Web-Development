ebfabdd4247c6ebf3b972f8ac3579bf7d2fd5622
Aenimen <songuyen150@gmail.com>
2025-04-10 19:02:10 -0700

    Added functions for each type of question

--- src/Questions.java
+++ src/Questions.java
@@ -1,9 +1,26 @@
 
 public class Questions {
 	
+	private String questions;
 	private GamePane gameScreen;
 	
 	public Questions(GamePane gameScreen) {
 		this.gameScreen = gameScreen;
+	}
+	
+	public String additionQuestion() {
+		return "";
+	}
+	
+	public String subtractionQuestion() {
+		return "";
+	}
+	
+	public String multiplicationQuestion() {
+		return "";
+	}
+	
+	public String divisionQuestion() {
+		return "";
 	}
 }
