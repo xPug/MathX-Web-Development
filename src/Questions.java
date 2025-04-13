36bfda9e9290006d5d2184cb70918b8d6f78f822
Aenimen <songuyen150@gmail.com>
2025-04-13 16:51:59 -0700

    Added random number generator for value of digits

--- src/Questions.java
+++ src/Questions.java
@@ -1,11 +1,23 @@
+import java.util.Random;
 
 public class Questions {
 	private QuestionType type;
+	private int valueOne;
+	private int valueTwo;
 	
 	public Questions(QuestionType type) {
 		this.type = type;
 		
 	}
+	
+	public void oneDigitGenerator() {
+	    valueOne = (int)(Math.random() * 10 + 1);
+	}
+	
+	public void twoDigitGenerator() {
+		valueTwo = (int)(Math.random() * 100 + 1);
+	}
+	
 	
 	public String additionQuestion() {
 		return "";
