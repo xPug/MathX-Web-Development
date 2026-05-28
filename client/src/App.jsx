13120c1fd00dd5604e2d6186eafdb81310476176
Aenimen <songuyen150@gmail.com>
2026-05-28 14:16:05 -0700

    added new state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,6 +39,7 @@ function App() {
     const [playerData, setPlayerData] = useState(null);
     const [leaderboard, setLeaderboard] = useState([]);
     const [players, setPlayers] = useState({});
+    const [showMatchIntro, setShowMatchIntro] = useState(false);
 
     useEffect(() => {
 
